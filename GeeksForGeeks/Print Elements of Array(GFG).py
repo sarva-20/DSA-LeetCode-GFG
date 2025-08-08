815d9dedebef77ac17c74413b1f7c02f1a47967b
Sarvatarshan Sankar <92633836+sarva-20@users.noreply.github.com>
2025-08-08 21:36:51 +0530

    Add files via upload

---
+++ GeeksForGeeks/Print Elements of Array(GFG).py
@@ -0,0 +1,3 @@
+class Solution:
+    def printArray(self, arr):
+        print(*[i for i in arr], end=' ')
---
+++ GeeksForGeeks/SumOfArray(GFG).py
@@ -0,0 +1,14 @@
+#User function Template for python3
+class Solution:
+	def arraySum(self, arr):
+   		# code here
+   		return sum(arr)
+
+'''
+class Solution:
+    def sumOfArray(self, arr):
+        total = 0
+        for num in arr:
+            total += num
+        return total
+'''
