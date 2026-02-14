ea9202f08fc374678a3bee67caa9e91f31fe0dac
Sarvatarshan Sankar <92633836+sarva-20@users.noreply.github.com>
2026-02-14 13:43:12 +0530

    Time: 6 ms (15.19%), Space: 47.8 MB (84.45%) - LeetHub

---
+++ 0799-champagne-tower/0799-champagne-tower.java
@@ -0,0 +1,35 @@
+class Solution {
+    double[][] memo = new double[101][101];
+
+    public double solve(int poured, int i, int j){
+        if(i<0 || j>i || j<0){
+            return 0.0;
+        }
+        if(i==0 && j==0){
+            return memo[i][j]=poured;
+        }
+        if(memo[i][j]!=-1){
+            return memo[i][j];
+        }
+        double up_left = (solve(poured,i-1,j-1)-1)/2.0;
+        double up_right = (solve(poured,i-1,j)-1)/2.0;
+
+        if(up_left<0){
+            up_left=0.0;
+        }
+        if(up_right<0){
+            up_right=0.0;
+        }
+        return memo[i][j]=up_right+up_left;
+    }
+
+
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        for(int i=0;i<101;i++){
+            for(int j=0;j<101;j++){
+                memo[i][j]=-1;
+            }
+        }
+        return Math.min(1.0,solve(poured, query_row, query_glass));
+    }
+}
